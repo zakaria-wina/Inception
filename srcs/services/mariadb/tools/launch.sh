07ed5f282588114e46890d6701dd696222831d0e
Zakaria Wina <zwina@e2r11p7.1337.ma>
2023-06-09 15:40:55 +0100

    INCEPTION fix redis cache

--- srcs/services/mariadb/tools/launch.sh
+++ srcs/services/mariadb/tools/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's/^bind-address\s*=.*/bind-address\t\t= 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
--- srcs/services/wordpress/tools/launch.sh
+++ srcs/services/wordpress/tools/launch.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-wp core download --allow-root --path=/var/www/html
+cd /var/www/html
 
-sed -i 's/\/run\/php\/php7.3-fpm.sock/9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/\/run\/php\/php7.3-fpm.sock/wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-wp config create --allow-root --path=/var/www/html \
-                --dbname=$MARIADB_NAME \
-                --dbuser=$MARIADB_USER \
-                --dbpass=$MARIADB_PASS \
-                --dbhost=mariadb
+wp core download --path=/var/www/html --allow-root
 
-wp core install --allow-root --path=/var/www/html \
-                --url=$WORDPRESS_URL \
-                --title=$WORDPRESS_TITLE \
-                --admin_user=$WORDPRESS_ADMIN_USER \
-                --admin_password=$WORDPRESS_ADMIN_PASS \
-                --admin_email=$WORDPRESS_ADMIN_EMAIL
+wp config create --path=/var/www/html --allow-root \
+    --dbname=$MARIADB_NAME \
+    --dbuser=$MARIADB_USER \
+    --dbpass=$MARIADB_PASS \
+    --dbhost=$MARIADB_HOST
 
-wp user create --allow-root --path=/var/www/html \
-                $WORDPRESS_USER $WORDPRESS_EMAIL \
-                --user_pass=$WORDPRESS_PASS \
-                --role=author
+wp core install --path=/var/www/html --allow-root \
+    --url=$WORDPRESS_URL \
+    --title=$WORDPRESS_TITLE \
+    --admin_user=$WORDPRESS_ADMIN_USER \
+    --admin_password=$WORDPRESS_ADMIN_PASS \
+    --admin_email=$WORDPRESS_ADMIN_EMAIL
 
-wp plugin install --allow-root --path=/var/www/html redis-cache --activate
-wp config set --allow-root --path=/var/www/html WP_REDIS_HOST redis
-wp config set --allow-root --path=/var/www/html WP_REDIS_PORT 6379
-wp config set --allow-root --path=/var/www/html WP_CACHE true
-wp redis enable --allow-root --path=/var/www/html
+wp user create --path=/var/www/html --allow-root \
+    $WORDPRESS_USER $WORDPRESS_EMAIL \
+    --user_pass=$WORDPRESS_PASS \
+    --role=author
 
-php-fpm7.3 -R -F
+wp plugin install --path=/var/www/html --allow-root redis-cache --activate
+wp config set FS_METHOD direct    --type=constant --path=/var/www/html --allow-root
+wp config set WP_REDIS_HOST redis --type=constant --path=/var/www/html --allow-root
+wp config set WP_REDIS_PORT 6379  --type=constant --path=/var/www/html --allow-root
+wp config set WP_CACHE true       --type=constant --path=/var/www/html --allow-root
+wp redis enable --path=/var/www/html --allow-root
+
+chown -R www-data:www-data /var/www/html
+
+php-fpm7.3 -F -R
